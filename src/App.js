cb1aa698bd4b66b611e9697e810fc01c93c8f1b5
Hui Cao <caohuiqaz@gmail.com>
2020-08-19 12:28:18 +1000

    updating app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE </h1>
-    </div>
-);
 
 function App() {
     return (
@@ -17,7 +13,7 @@ function App() {
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/hats' component={HatsPage} />
+                <Route path='/shop' component={ShopPage} />
             </Switch>
         </div>
         </BrowserRouter>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size, history, match, linkUrl}) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    
     <div className={`${size} menu-item`} 
-    onclick={() => history.push(`${match.url}${linkUrl}`)}>
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+
         <div
             className='background-image'
             style={{
