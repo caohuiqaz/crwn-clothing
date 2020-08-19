c68b3d40e721390f4621d2a5cb7d9e63165d8d51
Hui Cao <caohuiqaz@gmail.com>
2020-08-19 15:31:06 +1000

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 
 function App() {
     return (
-        <BrowserRouter>
         <div>
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
-            </Switch>
+            <BrowserRouter>
+                <Header />
+                
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                </Switch>
+            </BrowserRouter>
         </div>
-        </BrowserRouter>
     );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss';
 
+// import {Link} from "react-router-dom";
+
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     
     <div className={`${size} menu-item`} 
         onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
+    {/* <Link className={`${size} menu-item`} to={`${match.url}${linkUrl}`}>  */}
 
         <div
             className='background-image'
@@ -19,6 +22,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
           <h1 className='title'>{title.toUpperCase()}</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
+    
     </div>
 )
 
