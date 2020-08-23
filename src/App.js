c0cec487cbe6829ee57f847217b958afe4d7fbe8
Hui Cao <caohuiqaz@gmail.com>
2020-08-23 21:48:52 +1000

    updating app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -24,11 +24,10 @@ class App extends React.Component {
 
                 userRef.onSnapshot(snapShot => {
                     this.props.setCurrentUser({
-                            id: snapShot.id,
-                            ...snapShot.data()
-                        });
+                        id: snapShot.id,
+                        ...snapShot.data()
                     });
-                
+                });
             }
 
             setCurrentUser(userAuth);
@@ -48,7 +47,13 @@ class App extends React.Component {
                     <Switch>
                         <Route exact path='/' component={HomePage} />
                         <Route path='/shop' component={ShopPage} />
-                        <Route path='/signin' component={SignInAndSignUpPage} />
+                        <Route exact path='/signin' 
+                            render={() => this.props.currentUser ? (
+                            <Redirect to='/' />
+                            ) : (
+                            <SignInAndSignUpPage />
+                            )}
+                        />
                     </Switch>
                 </BrowserRouter>
             </div>
@@ -56,8 +61,15 @@ class App extends React.Component {
     }
 }
 
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps )(App);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
